5e2dbfc5f431ecbf3ea853db19822dc67a8262a9
jsjin7371 <120244130+jsjin7371@users.noreply.github.com>
2024-11-21 18:54:34 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void* a, const void* b){
+    return *(int*)a > *(int*)b;
+};
+
+int main(){
+    int n, ans = 0;
+    scanf("%d", &n);
+    
+    int* arr = (int*)malloc(sizeof(int)*n);
+
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    
+    qsort(arr,n,sizeof(int),compare);
+
+    for(int i=0; i<n; i++){
+        ans += arr[i]*(n-i);
+    }
+
+    printf("%d", ans);
+
+    free(arr);
+
+    return 0;
+}
